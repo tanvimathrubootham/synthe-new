f5a94ec4581ff7ee189da116d4b6e692dc0b39f8
Tanvi Mathrubootham <112732223+tanvimathrubootham@users.noreply.github.com>
2026-03-09 08:19:30 -0700

    Create PageNotFound.jsx

---
+++ src/lib/PageNotFound.jsx
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-semibold">404 - Page Not Found</h1>
+    </div>
+  );
+}
